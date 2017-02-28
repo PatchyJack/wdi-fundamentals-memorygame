da82aff92c79b3e8bae8e41e049dc2e46505fc83
Jack McGregor <jackjwmcgregor@gmail.com>
2017-02-28 14:23:06 +0000

    added second JS

--- starter-code/main.js
+++ starter-code/main.js
@@ -7,8 +7,20 @@ var cardFour = "queen";
 
 // First edit from instructions
 
-if (cardTwo === cardFour) {
+/* if (cardTwo === cardFour) {
 	alert("You found a match!");
-} else {
-	alert("Sorry, try again.")
-};
+} 
+	alert("Sorry, try again."); */
+
+var gameBoard = document.getElementById('game-board');
+
+var createCards = function() {
+  for (i = 0; i < 4; i++) { 
+    var cardsElement = document.createElement('div');
+    cardsElement.className = 'card';
+    gameBoard.appendChild(cardsElement)
+    }    
+} 
+
+createCards();
+
